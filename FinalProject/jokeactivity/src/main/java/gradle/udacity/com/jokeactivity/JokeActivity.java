378e6b53e558bf5ab73a4bcf080fffbe22fec24c
Parto Karwat <partokarwat@gmail.com>
2017-03-13 17:55:25 +0100

    Add joke displaying Text View to Android library Activity

--- FinalProject/jokeactivity/src/main/java/gradle/udacity/com/jokeactivity/JokeActivity.java
+++ FinalProject/jokeactivity/src/main/java/gradle/udacity/com/jokeactivity/JokeActivity.java
@@ -1,7 +1,9 @@
 package gradle.udacity.com.jokeactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class JokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
+        // The joke Activity called via intent.  Inspect the intent for joke data.
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String jokeStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) findViewById(R.id.joke_text_view))
+                    .setText(jokeStr);
+        }
     }
 }

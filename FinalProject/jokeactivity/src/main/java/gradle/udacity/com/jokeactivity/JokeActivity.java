cd649eec4a4c5b116aaaa6685fcffe48abdca099
Parto Karwat <partokarwat@gmail.com>
2017-03-30 12:00:59 +0200

    Revert Interstitial Ad

--- FinalProject/jokeactivity/src/main/java/gradle/udacity/com/jokeactivity/JokeActivity.java
+++ FinalProject/jokeactivity/src/main/java/gradle/udacity/com/jokeactivity/JokeActivity.java
@@ -5,11 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-
 public class JokeActivity extends AppCompatActivity {
-    private InterstitialAd mInterstitial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +20,5 @@ public class JokeActivity extends AppCompatActivity {
                     .setText(jokeStr);
         }
 
-        // Load the interstitial that should be shown on back pressed
-        mInterstitial = new InterstitialAd(this);
-        mInterstitial.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        AdRequest request = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mInterstitial.loadAd(request);
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        if (mInterstitial.isLoaded()) {
-            mInterstitial.show();
-        }
     }
 }
